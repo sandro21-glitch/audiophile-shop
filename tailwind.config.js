a013e75c7f655909342dc17639f296cf9309c8f9
Sandro <vipsandro11@gmail.com>
2023-11-03 23:05:46 +0400

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#191919",
+        "orange-brown": "#d87d4a",
+        "orange-brown-light": "#fbaf85",
+        textLight: "rgba(255,255,255,0.5)",
+        "text-white": "#fff",
+        text: "rgba(0,0,0,0.5)",
+      },
+    },
   },
   plugins: [],
-}
+};
