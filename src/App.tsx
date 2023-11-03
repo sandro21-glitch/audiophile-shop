5d4488fdba20da8ec2f98839470c30e965edbee5
Sandro <vipsandro11@gmail.com>
2023-11-03 23:05:54 +0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-
+import data from "./data/products.json";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./ui/Navbar";
 function App() {
-
+  console.log(data);
   return (
-   <div>
-    Test
-   </div>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
