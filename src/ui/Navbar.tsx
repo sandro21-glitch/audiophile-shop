aa134af654b5c473adc088205f3b0c22ae0b37bc
Sandro <vipsandro11@gmail.com>
2023-11-03 23:22:46 +0400

    initial commit

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,7 +1,33 @@
+import { Link } from "react-router-dom";
+import logo from "/assets/logo.svg";
+import cart from "/assets/icon-cart.svg";
 const Navbar = () => {
   return (
-    <header className="w-full">
-      <nav className="section-center bg-primary-color"></nav>
+    <header className="w-full bg-primary-color py-10">
+      <nav className="section-center text-text-white">
+        <div className="flex justify-between">
+          <button>
+            <img src={logo} alt="logo" />
+          </button>
+          <ul className="flex-center gap-8 font-medium ">
+            <li className="text-[0.875rem] hover:text-orange-brown transition-colors ease-in duration-150 tracking-wider">
+              <Link to="/">HOME</Link>
+            </li>
+            <li className="text-[0.875rem] hover:text-orange-brown transition-colors ease-in duration-150 tracking-wider">
+              <Link to="/">HEADPHONES</Link>
+            </li>
+            <li className="text-[0.875rem] hover:text-orange-brown transition-colors ease-in duration-150 tracking-wider">
+              <Link to="/">SPEAKERS</Link>
+            </li>
+            <li className="text-[0.875rem] hover:text-orange-brown transition-colors ease-in duration-150 tracking-wider">
+              <Link to="/">EARPHONES</Link>
+            </li>
+          </ul>
+          <button>
+            <img src={cart} alt="cart" />
+          </button>
+        </div>
+      </nav>
     </header>
   );
 };
