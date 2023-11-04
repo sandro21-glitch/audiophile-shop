df9499dbaa54475680e8770a8dee2a3807bc959b
Sandro <vipsandro11@gmail.com>
2023-11-04 19:20:35 +0400

    refactor: change navbar styles

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -3,7 +3,7 @@ import logo from "/assets/logo.svg";
 import cart from "/assets/icon-cart.svg";
 const Navbar = () => {
   return (
-    <header className="w-full bg-primary-color py-10">
+    <header className="w-full bg-primary-color py-10 px-5 relative after:absolute after:w-[72.375rem] after:mx-auto after:left-0 after:right-0 after:h-[1px] after:bg-divider after:bottom-0 ">
       <nav className="section-center text-text-white">
         <div className="flex justify-between">
           <button>
