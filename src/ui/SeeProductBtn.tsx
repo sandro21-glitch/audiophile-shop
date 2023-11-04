2cac766753a2305161087e7da8a1509bc72b88fb
Sandro <vipsandro11@gmail.com>
2023-11-04 19:20:58 +0400

    feat: create reusable button component

---
+++ src/ui/SeeProductBtn.tsx
@@ -0,0 +1,9 @@
+const SeeProductBtn = () => {
+  return (
+    <button className="bg-orange-brown px-8 py-3 text-white font-semibold uppercase text-[.9rem] hover:bg-orange-brown-light transition-colors ease-in duration-150">
+      See Product
+    </button>
+  );
+};
+
+export default SeeProductBtn;
