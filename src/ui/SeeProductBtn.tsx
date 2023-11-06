9b428751e90b5eeb9f74611474253fc486b0c864
Sandro <vipsandro11@gmail.com>
2023-11-06 17:29:06 +0400

    feat: Refactor SeeProductBtn component to accept a color prop

--- src/ui/SeeProductBtn.tsx
+++ src/ui/SeeProductBtn.tsx
@@ -1,6 +1,13 @@
-const SeeProductBtn = () => {
+type ButtonTypes = {
+  color: string;
+};
+
+const SeeProductBtn = ({ color }: ButtonTypes) => {
   return (
-    <button className="bg-orange-brown px-8 py-3 text-white font-semibold uppercase text-[.9rem] hover:bg-orange-brown-light transition-colors ease-in duration-150">
+    <button
+      style={{ backgroundColor: color }}
+      className={` px-8 py-3 text-white font-semibold uppercase text-[.9rem] hover:opacity-80 transition-all ease-in duration-150`}
+    >
       See Product
     </button>
   );
