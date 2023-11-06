27cc5a3c155a0ad97b92b9a83eb28008f96c6bd5
Sandro <vipsandro11@gmail.com>
2023-11-06 17:29:21 +0400

    feat: Add SingleProductLink component to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import HeroPage from "../features/home/HeroPage";
 import Products from "../features/home/Products";
+import SingleProductLink from "../features/home/SingleProductLink";
 
 const Home = () => {
   return (
     <section>
       <HeroPage />
-      <div className="section-center">
+      <div className="section-center mx-5">
         <Products />
+        <SingleProductLink />
       </div>
     </section>
   );
