dcc112317b1991e63ea5686fc21fcb44b34dfe03
Sandro <vipsandro11@gmail.com>
2023-11-07 22:51:27 +0400

    refactor: Replace SingleProductLink with individual product card components

--- src/features/home/SingleProductLink.tsx
+++ src/features/home/SingleProductLink.tsx
@@ -1,35 +1,13 @@
-import SeeProductBtn from "../../ui/SeeProductBtn";
-import speaker from "/assets/image-speaker-zx9.png";
-// import circles from "/assets/pattern-circles.svg";
+import SpeakerProductCard from "./cards/SpeakerProductCard";
+import SpeakerZx7ProductCard from "./cards/SpeakerZx7ProductCard";
+import EarphonesProductCart from "./cards/EarphonesProductCart";
 const SingleProductLink = () => {
   return (
     <div className="mt-28 mb-20">
-      <ul>
-        <li
-          className="bg-orange-brown pb-0 pt-20 overflow-hidden bg-no-repeat bg-cover rounded-lg"
-          style={{
-            backgroundImage: 'url("/assets/pattern-circles.svg")',
-            backgroundPosition: "-15rem -9.125rem",
-          }}
-        >
-          <div className="flex">
-            <img
-              src={speaker}
-              alt=""
-              className="w-[25.625rem] ml-[7rem] relative top-[0.8rem]"
-            />
-            <div className="text-left ml-36 mt-[2rem]">
-              <h2 className="mb-7 text-[4rem] text-white font-semibold">
-                ZX9 <br /> SPEAKER
-              </h2>
-              <p className="mb-7 max-w-[20rem] font-thin text-[.9rem]">
-                Upgrade to premium speakers that are phenomenally built to
-                deliver truly remarkable sound.
-              </p>
-              <SeeProductBtn color="black" />
-            </div>
-          </div>
-        </li>
+      <ul className="w-full">
+        <SpeakerProductCard />
+        <SpeakerZx7ProductCard />
+        <EarphonesProductCart />
       </ul>
     </div>
   );
