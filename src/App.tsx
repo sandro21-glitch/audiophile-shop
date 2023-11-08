7ab1ce322b60e6541f63f4e769bf05be64a0655a
Sandro <vipsandro11@gmail.com>
2023-11-08 16:53:27 +0400

    refactor:  load product data and add 'Hadphones' route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import data from "./data/products.json";
+// import data from "./data/products.json";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./ui/Navbar";
 import Footer from "./ui/Footer";
+import Hadphones from "./features/headphones/Hadphones";
+import { useAppDispatch } from "./hooks/reduxHooks";
+import { useEffect } from "react";
+import { getProductData } from "./dataUtils";
+import { setProductData } from "./features/products/productsSlice";
 function App() {
-  console.log(data);
+  const productData = getProductData();
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(setProductData(productData));
+  }, []);
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/headphones" element={<Hadphones />} />
       </Routes>
       <Footer />
     </BrowserRouter>
