0f1a47abf47494f70b560ca76911b34b405cd5a2
Sandro <vipsandro11@gmail.com>
2023-11-10 12:06:55 +0400

    Refactor: Adjust spacing in Footer component

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
       <nav className="section-center text-text-white py-20 px-5
        relative after:w-[6rem] after:h-1 after:bg-orange-brown after:absolute after:left-5 after:top-0">
         <div className="flex flex-col lg:flex-row items-start justify-between lg:items-center">
-          <button className="mb-5 lg:mb-0">
+          <button className="mt-10 mb-5 lg:mb-0">
             <img src={logo} alt="logo" />
           </button>
           <ul className="flex-center gap-8 font-medium ">
@@ -52,7 +52,7 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <p className="mt-10 text-textLight text-[.9rem]">
+        <p className="mt-10 text-textLight text-[.9rem] text-center lg:text-left">
           Copyright 2021. All Rights Reserved
         </p>
       </nav>
