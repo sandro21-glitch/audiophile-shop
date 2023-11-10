b420960c37080aa68046f47a0ed6a084c747b3ed
Sandro <vipsandro11@gmail.com>
2023-11-10 12:07:30 +0400

    Refactor: Improve responsive layout in SingleHeadphone component

--- src/ui/SingleItem.tsx
+++ src/ui/SingleItem.tsx
@@ -1,43 +1,44 @@
 import Button from "./SeeProductBtn";
 import { Product } from "../features/products/productsSlice";
+
 interface SingleHeadphoneProps {
   singleProduct: Product;
   index: number;
 }
 
-const SingleHeadphone = ({ singleProduct, index  }: SingleHeadphoneProps) => {
-  console.log(singleProduct)
+const SingleHeadphone = ({ singleProduct, index }: SingleHeadphoneProps) => {
   return (
-    <li className={`flex items-center ${index == 1 ? "flex-row-reverse" : ""}`}>
-      <div className={`flex-1 ${index == 1 ? "ml-[7rem]" : "mr-[7rem]"}`}>
+    <li
+      className={`flex items-center ${
+        index === 1 ? "lg:flex-row-reverse" : "lg:flex-row"
+      } flex-col`}
+    >
+      <div className={`flex-1 ${index === 1 ? "lg:ml-[7rem]" : "lg:mr-[7rem]"} w-full mb-7`}>
         <img
           src={singleProduct.image.mobile}
           alt={singleProduct.name}
-          className="block md:hidden"
+          className="block md:hidden w-full h-full object-cover"
         />
         <img
-          src={singleProduct.image.tablet}
+          src={singleProduct.categoryImage.tablet}
           alt={singleProduct.name}
-          className="hidden md:block lg:hidden"
+          className="hidden md:block lg:hidden h-full object-cover"
         />
         <img
-          src={singleProduct.image.desktop}
+          src={singleProduct.categoryImage.desktop}
           alt={singleProduct.name}
-          className="hidden lg:block"
+          className="hidden lg:block h-full object-cover"
         />
       </div>
-
-      <div className="flex-1">
+      <div className="flex-1 text-center lg:text-left">
         <h2 className="text-[2rem] font-medium mb-7">
-          {singleProduct.new ? (
+          {singleProduct.new && (
             <strong className="text-orange-brown block text-[1rem] uppercase tracking-[.5em] font-normal">
               New Product
             </strong>
-          ) : (
-            ""
           )}
           {singleProduct.new && <br />}
-          <span className="max-w-[250px] block break-words">
+          <span className="max-w-[250px] mx-auto lg:mx-0 block break-words">
             {singleProduct.name}
           </span>
         </h2>
