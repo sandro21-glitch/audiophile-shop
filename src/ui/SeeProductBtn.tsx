703f409534e2fe8fbf15ab2512c44ffd2029c790
Sandro <vipsandro11@gmail.com>
2023-11-11 22:07:33 +0400

    feat: Update Button component to handle navigation

--- src/ui/SeeProductBtn.tsx
+++ src/ui/SeeProductBtn.tsx
@@ -1,23 +1,38 @@
+import { Link, useLocation } from "react-router-dom";
+
 interface ButtonProps {
   children: React.ReactNode;
   colorClass?: string;
   bgClass?: string;
   hoverClass?: string;
   text?: string;
+  prodId: number;
 }
+
 const Button = ({
   children,
   colorClass,
   bgClass,
   hoverClass,
   text,
+  prodId,
 }: ButtonProps) => {
-  const buttonClasses = `
-    mb-5 px-8 py-3
-     font-semibold uppercase transition ease-in duration-150
-    text-[.9rem] ${bgClass} text-${text} ${colorClass} ${hoverClass}`;
+  const { pathname } = useLocation();
 
-  return <button className={buttonClasses}>{children}</button>;
+  return (
+    <button
+      className={`mb-5 px-8 py-3 font-semibold uppercase transition ease-in duration-150 text-[.9rem] ${bgClass} text-${text} ${colorClass} ${hoverClass}`}
+    >
+      <Link
+        to={`${pathname}/${encodeURIComponent(prodId)
+          .replace(/%/g, "-")
+          .replace(/20/g, "")
+          .toLowerCase()}`}
+      >
+        {children}
+      </Link>
+    </button>
+  );
 };
 
 export default Button;
