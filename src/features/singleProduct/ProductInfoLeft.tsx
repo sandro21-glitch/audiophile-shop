29e2a4a2f319c40b4bd6c112dcbab0aabb03b5fc
Sandro <vipsandro11@gmail.com>
2023-11-12 20:12:50 +0400

    feat: Enhance responsive images in ProductInfoLeft component

---
+++ src/features/singleProduct/ProductInfoLeft.tsx
@@ -0,0 +1,32 @@
+type ProdInfoTyps = {
+  image: {
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+  name: string;
+};
+
+const ProductInfoLeft = ({ image, name }: ProdInfoTyps) => {
+  return (
+    <div className="flex-1 mr-[2rem] lg:mr-[7rem]">
+      <img
+        src={image.mobile}
+        alt={name}
+        className="block md:hidden w-full h-full object-cover rounded-lg"
+      />
+      <img
+        src={image.tablet}
+        alt={name}
+        className="hidden md:block lg:hidden h-full object-cover rounded-lg"
+      />
+      <img
+        src={image.desktop}
+        alt={name}
+        className="hidden lg:block h-full object-cover rounded-lg"
+      />
+    </div>
+  );
+};
+
+export default ProductInfoLeft;
