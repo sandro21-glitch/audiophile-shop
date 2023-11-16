4495a3879c85cdb3231fd9b247f41a8aefd48857
Sandro <vipsandro11@gmail.com>
2023-11-16 22:28:51 +0400

    style: Improve styling in ProductGalerry

--- src/features/singleProduct/ProductGalerry.tsx
+++ src/features/singleProduct/ProductGalerry.tsx
@@ -2,22 +2,22 @@ import { Product } from "../products/productsSlice";
 
 const ProductGalerry = ({ singleProduct }: { singleProduct: Product }) => {
   return (
-    <div className="grid grid-cols-2 gap-5">
+    <div className="grid grid-cols-2 gap-5 mb-20">
       <div className="col-span-1">
         <img
-          className="w-full"
+          className="w-full rounded-lg"
           src={singleProduct.gallery.first.desktop}
           alt=""
         />
         <img
-          className="w-full mt-5"
+          className="w-full mt-5 rounded-lg"
           src={singleProduct.gallery.second.desktop}
           alt=""
         />
       </div>
       <div className="col-span-1">
         <img
-          className="w-full h-full"
+          className="w-full h-full rounded-lg"
           src={singleProduct.gallery.third.desktop}
           alt=""
         />
