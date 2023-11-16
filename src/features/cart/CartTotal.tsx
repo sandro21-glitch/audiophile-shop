ad12f962e0e448edc93ca3deeddbaaf83d615480
Sandro <vipsandro11@gmail.com>
2023-11-16 22:30:21 +0400

    feat: Add CartTotal component

---
+++ src/features/cart/CartTotal.tsx
@@ -0,0 +1,10 @@
+const CartTotal = () => {
+  return (
+    <div className="flex justify-between mb-5">
+      <p className="text-black">TOTAL</p>
+      <p className="text-black">$ 899</p>
+    </div>
+  );
+};
+
+export default CartTotal;
