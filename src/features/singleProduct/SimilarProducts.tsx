35ea0e6bb2d68efb42043dfbb93906798f96338c
Sandro <vipsandro11@gmail.com>
2023-11-16 22:28:33 +0400

    feat: Add SimilarProducts component

---
+++ src/features/singleProduct/SimilarProducts.tsx
@@ -0,0 +1,34 @@
+import { useAppSelector } from "../../hooks/reduxHooks";
+import SeeProductBtn from "../../ui/SeeProductBtn";
+
+const SimilarProducts = () => {
+  const product = useAppSelector((store) => store.product.product);
+  return (
+    <div className="flex gap-7 ">
+      {product.slice(0, 3).map((singleProduct) => {
+        return (
+          <div>
+            <div className="bg-color-gray flex-1 rounded-md p-4">
+              <img src={singleProduct.image.desktop} alt="" />
+            </div>
+            <div className="text-center">
+              <h4 className="text-center my-5 font-medium">
+                {singleProduct.name}
+              </h4>
+              <SeeProductBtn
+                bgClass="bg-orange-brown"
+                hoverClass="hover:bg-orange-brown-light"
+                text="white"
+                prodId={singleProduct.id}
+              >
+                see product
+              </SeeProductBtn>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default SimilarProducts;
