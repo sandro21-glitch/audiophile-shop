f910101b7b79b64a9fc52a76b9f90019661ca994
Sandro <vipsandro11@gmail.com>
2023-11-17 23:28:22 +0400

    style: Update CartHeader component styles and button appearance

--- src/features/cart/CartHeader.tsx
+++ src/features/cart/CartHeader.tsx
@@ -1,14 +1,14 @@
-
-
 const CartHeader = () => {
   return (
     <header className="flex justify-between w-full mb-10">
-              <h5 className="text-black mb-0 uppercase font-medium text-[1.1rem]">
-                Cart <span>(1)</span>
-              </h5>
-              <button className="underline text-text">Remove All</button>
-            </header>
-  )
-}
+      <h6 className="text-black mb-0 uppercase font-medium text-[1.1rem]">
+        Cart <span>(1)</span>
+      </h6>
+      <button className="underline text-text text-[.9rem] hover:text-orange-brown transition-colors ease-in duration-150">
+        Remove All
+      </button>
+    </header>
+  );
+};
 
-export default CartHeader
+export default CartHeader;
