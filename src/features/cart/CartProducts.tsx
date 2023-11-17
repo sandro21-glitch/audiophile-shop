cb22ed8d60d1e57fac43a9d6744edde4932e9cc8
Sandro <vipsandro11@gmail.com>
2023-11-17 23:27:21 +0400

    feat: Update CartProducts component structure

--- src/features/cart/CartProducts.tsx
+++ src/features/cart/CartProducts.tsx
@@ -1,18 +1,13 @@
+import CartCounter from "./CartCounter";
+import CartSingleProduct from "./CartSingleProduct";
+
 const CartProducts = () => {
   return (
-    <div className="mb-6">
-      <div className="flex items-center gap-2">
-        <img
-          src="https://audiophile-ecommerce-mbart13.vercel.app/_next/image?url=%2Fimages%2Fcart%2Fimage-xx59-headphones.jpg&w=64&q=75"
-          alt=""
-          className="rounded-md"
-        />
-        <div>
-          <p className=" text-black text-[1rem]">XX59</p>
-          <p className="font-medium text-text text-[.9rem]">$ 899</p>
-        </div>
-      </div>
-      {/* here counter */}
+    <div className="mb-6 w-full flex items-center justify-between ">
+      {/* single product */}
+      <CartSingleProduct />
+      {/* single product counter */}
+      <CartCounter />
     </div>
   );
 };
