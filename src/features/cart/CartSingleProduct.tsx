c21b153c978dd8cd86af651c7c091f3842d005b3
Sandro <vipsandro11@gmail.com>
2023-11-17 23:27:04 +0400

    feat: Create CartSingleProduct component

---
+++ src/features/cart/CartSingleProduct.tsx
@@ -0,0 +1,16 @@
+const CartSingleProduct = () => {
+  return (
+    <div className="flex items-center gap-2">
+      <img
+        src="https://audiophile-ecommerce-mbart13.vercel.app/_next/image?url=%2Fimages%2Fcart%2Fimage-xx59-headphones.jpg&w=64&q=75"
+        alt=""
+        className="rounded-md"
+      />
+      <div>
+        <p className=" text-black text-[1rem]">XX59</p>
+        <p className="font-medium text-text text-[.9rem]">$ 899</p>
+      </div>
+    </div>
+  );
+};
+export default CartSingleProduct
