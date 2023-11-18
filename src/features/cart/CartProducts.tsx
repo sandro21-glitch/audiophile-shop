5e2c92ef500e5899bfefd87fcb448fb93429bfa0
Sandro <vipsandro11@gmail.com>
2023-11-18 17:25:06 +0400

    refactor: Dynamically render CartSingleProduct components based on cart items

--- src/features/cart/CartProducts.tsx
+++ src/features/cart/CartProducts.tsx
@@ -1,11 +1,15 @@
+import { useAppSelector } from "../../hooks/reduxHooks";
 import CartCounter from "./CartCounter";
 import CartSingleProduct from "./CartSingleProduct";
 
 const CartProducts = () => {
+  const cart = useAppSelector((store) => store.cart.cart);
   return (
     <div className="mb-6 w-full flex items-center justify-between ">
       {/* single product */}
-      <CartSingleProduct />
+      {cart.map((cartItem) => {
+        return <CartSingleProduct cartItem={cartItem} />;
+      })}
       {/* single product counter */}
       <CartCounter />
     </div>
