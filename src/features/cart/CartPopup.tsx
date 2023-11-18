2ffd398b669bec6bac76c91a9b34aaf29eedd13a
Sandro <vipsandro11@gmail.com>
2023-11-18 17:24:54 +0400

    refactor: Add close functionality to CartPopup using redux action

--- src/features/cart/CartPopup.tsx
+++ src/features/cart/CartPopup.tsx
@@ -2,17 +2,21 @@ import CartHeader from "./CartHeader";
 import CartProducts from "./CartProducts";
 import CartTotal from "./CartTotal";
 import CartCheckout from "./CartCheckout";
-import { useAppSelector } from "../../hooks/reduxHooks";
+import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
+import { setOpenCart } from "./cartSlice";
 
 const CartPopup = () => {
   const openCart = useAppSelector((store) => store.cart.openCart);
-
+  const dispatch = useAppDispatch();
   if (!openCart) return null;
   return (
     <div className="w-screen h-screen fixed z-[9999] left-0 right-0 top-0 bottom-0 overflow-hidden">
-      <div className="bg-black absolute opacity-40 w-full h-full z-1"></div>
-      <div className="relative">
-        <div className="absolute right-[13rem] top-[8rem] bg-white max-w-[25rem] w-full rounded-lg">
+      <div
+        className="bg-black absolute opacity-40 w-full h-full z-1"
+        onClick={() => dispatch(setOpenCart(false))}
+      ></div>
+      <div className="relative ">
+        <div className="absolute scale-up-center right-[13rem] top-[8rem] bg-white max-w-[25rem] w-full rounded-lg">
           <div className="p-5">
             {/*  cart header*/}
             <CartHeader />
