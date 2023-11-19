bd0b22eaa99c1842f052046c4d603f94a925267c
Sandro <vipsandro11@gmail.com>
2023-11-20 00:44:44 +0400

    initial commit

--- src/features/cart/CartProducts.tsx
+++ src/features/cart/CartProducts.tsx
@@ -1,14 +1,17 @@
 import { useAppSelector } from "../../hooks/reduxHooks";
 import CartCounter from "./CartCounter";
-import CartSingleProduct from "./CartSingleProduct";
+// import CartSingleProduct from "./CartSingleProduct";
 
 const CartProducts = () => {
   const cart = useAppSelector((store) => store.cart.cart);
+  console.log(cart)
   return (
     <div className="mb-6 w-full flex items-center justify-between ">
       {/* single product */}
       {cart.map((cartItem) => {
-        return <CartSingleProduct cartItem={cartItem} />;
+        console.log(cartItem);
+        const { name } = cartItem.cartItem;
+        return <p>{name}</p>;
       })}
       {/* single product counter */}
       <CartCounter />
