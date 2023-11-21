3d8eb4426e4d2a114dac312bfa81bc4eef98774c
Sandro <vipsandro11@gmail.com>
2023-11-21 13:55:18 +0400

    refactor: update CartProducts to use CartSingleProduct component

--- src/features/cart/CartProducts.tsx
+++ src/features/cart/CartProducts.tsx
@@ -1,20 +1,17 @@
 import { useAppSelector } from "../../hooks/reduxHooks";
-import CartCounter from "./CartCounter";
-// import CartSingleProduct from "./CartSingleProduct";
+import CartSingleProduct from "./CartSingleProduct";
 
 const CartProducts = () => {
   const cart = useAppSelector((store) => store.cart.cart);
-  console.log(cart)
+  if (!cart) return null;
   return (
     <div className="mb-6 w-full flex items-center justify-between ">
       {/* single product */}
-      {cart.map((cartItem) => {
-        console.log(cartItem);
-        const { name } = cartItem.cartItem;
-        return <p>{name}</p>;
-      })}
-      {/* single product counter */}
-      <CartCounter />
+      <div className="w-full">
+        {cart.map((cartItem) => {
+          return <CartSingleProduct key={cartItem.id} cartItem={cartItem} />;
+        })}
+      </div>
     </div>
   );
 };
