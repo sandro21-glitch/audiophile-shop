d25a40bbed38d3fedafff76bba8ba194b9d79452
Sandro <vipsandro11@gmail.com>
2023-11-22 23:12:05 +0400

    refactor: optimize CartPopup component structure for better modularity

--- src/features/cart/CartPopup.tsx
+++ src/features/cart/CartPopup.tsx
@@ -1,12 +1,11 @@
-import CartHeader from "./CartHeader";
-import CartProducts from "./CartProducts";
-import CartTotal from "./CartTotal";
-import CartCheckout from "./CartCheckout";
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
 import { setOpenCart } from "./cartSlice";
+import CartBody from "./CartBody";
+import EmptyCart from "./EmptyCart";
 
 const CartPopup = () => {
   const openCart = useAppSelector((store) => store.cart.openCart);
+  const cart = useAppSelector((store) => store.cart.cart);
   const dispatch = useAppDispatch();
   if (!openCart) return null;
   return (
@@ -15,20 +14,7 @@ const CartPopup = () => {
         className="bg-black absolute opacity-40 w-full h-full z-1"
         onClick={() => dispatch(setOpenCart(false))}
       ></div>
-      <div className="relative ">
-        <div className="absolute scale-up-center right-[13rem] top-[8rem] bg-white max-w-[25rem] w-full rounded-lg">
-          <div className="p-5">
-            {/*  cart header*/}
-            <CartHeader />
-            {/* cart items */}
-            <CartProducts />
-            {/* cart total */}
-            <CartTotal />
-            {/* CHECKOUT */}
-            <CartCheckout />
-          </div>
-        </div>
-      </div>
+      {cart.length < 1 ? <EmptyCart /> : <CartBody />}
     </div>
   );
 };
