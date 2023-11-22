b16d8d3a39b51a0a507b621806548b928668f778
Sandro <vipsandro11@gmail.com>
2023-11-22 23:11:16 +0400

    refactor: update CartTotal component to dynamically calculate and display total cart price

--- src/features/cart/CartTotal.tsx
+++ src/features/cart/CartTotal.tsx
@@ -1,8 +1,13 @@
+import { totalCartPrice } from "./cartSlice";
+import { formatPrice } from "../../utils/formatPrice";
+import { useAppSelector } from "../../hooks/reduxHooks";
+
 const CartTotal = () => {
+  const totalPrice = useAppSelector(totalCartPrice) ?? 0;
   return (
     <div className="flex justify-between mb-5">
       <p className="text-text font-normal">TOTAL</p>
-      <p className="text-black font-semibold">$ 899</p>
+      <p className="text-black font-semibold">{formatPrice(totalPrice)}</p>
     </div>
   );
 };
