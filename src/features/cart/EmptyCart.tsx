9c4c605ad3aa26aba6aea19439a5667381724c24
Sandro <vipsandro11@gmail.com>
2023-11-22 23:13:15 +0400

    feat: create EmptyCart component for displaying an empty cart message

---
+++ src/features/cart/EmptyCart.tsx
@@ -0,0 +1,23 @@
+import emptyCart from "/images/cart/empty-cart.png";
+const EmptyCart = () => {
+  return (
+    <div className="relative ">
+      <div className="absolute scale-up-center right-[13rem] top-[8rem] bg-white max-w-[25rem] w-full rounded-lg">
+        <div className="p-5">
+          <h6 className="py-10 text-center font-medium text-text">
+            Your cart is empty
+          </h6>
+          <div className="flex justify-center mb-5">
+            <img
+              src={emptyCart}
+              alt="empty cart"
+              className="w-[6rem] h-[6rem]"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmptyCart;
