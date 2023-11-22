271687d66d801a28190235266d324f49a61c2d43
Sandro <vipsandro11@gmail.com>
2023-11-22 23:11:32 +0400

    feat: expand cartSlice in Redux toolkit to include clearCart action and totalCartPrice selector

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -28,9 +28,29 @@ export const cartSlice = createSlice({
     addToCart: (state, action) => {
       state.cart.push(action.payload);
     },
+    clearCart: (state) => {
+      state.cart = [];
+    },
   },
 });
 
-export const { setOpenCart, addToCart } = cartSlice.actions;
+export const { setOpenCart, addToCart, clearCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const selectCart = (state: { cart: CartState }) => state.cart.cart;
+
+// total price
+export const totalCartPrice = (state: { cart: CartState }) => {
+  const cartItems = selectCart(state);
+
+  if (cartItems.length > 0) {
+    const totalPrice = cartItems.reduce(
+      (acc, item) => acc + item.price * item.amount,
+      0
+    );
+    return totalPrice;
+  } else {
+    console.log("Cart is empty");
+  }
+};
