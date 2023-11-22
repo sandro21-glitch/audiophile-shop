e9e505abb4729e7db957fd2e0c422d42ff05edee
Sandro <vipsandro11@gmail.com>
2023-11-22 23:11:00 +0400

    style: standardize code formatting in formatPrice function

--- src/utils/formatPrice.tsx
+++ src/utils/formatPrice.tsx
@@ -1,11 +1,11 @@
 export const formatPrice = (number: number) => {
-    const formattedPrice = new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 0,
-    }).format(number);
-  
-    // Remove the ".00" if present
-    return formattedPrice.replace(/\.00$/, '');
-  };
+  const formattedPrice = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(number);
+
+  // Remove the ".00" if present
+  return formattedPrice.replace(/\.00$/, "");
+};
