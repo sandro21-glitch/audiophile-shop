d4309ca5b1eb8f0e81899392f8ae97afe282dfda
Sandro <vipsandro11@gmail.com>
2023-11-22 23:13:04 +0400

    refactor: encapsulate CartBody content into individual components for modularity

---
+++ src/features/cart/CartBody.tsx
@@ -0,0 +1,25 @@
+import CartCheckout from "./CartCheckout";
+import CartHeader from "./CartHeader";
+import CartProducts from "./CartProducts";
+import CartTotal from "./CartTotal";
+
+const CartBody = () => {
+  return (
+    <div className="relative">
+      <div className="absolute scale-up-center right-[4rem] lg:right-[13rem] top-[8rem] bg-white max-w-[25rem] w-full rounded-lg">
+        <div className="p-5">
+          {/*  cart header*/}
+          <CartHeader />
+          {/* cart items */}
+          <CartProducts />
+          {/* cart total */}
+          <CartTotal />
+          {/* CHECKOUT */}
+          <CartCheckout />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartBody;
