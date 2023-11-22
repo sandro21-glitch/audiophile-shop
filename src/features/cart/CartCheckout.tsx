487c48aa14406dc78a89ee596f04f1fe66886be7
Sandro <vipsandro11@gmail.com>
2023-11-22 23:12:28 +0400

    style: enhance CartCheckout button styling for a more polished look

--- src/features/cart/CartCheckout.tsx
+++ src/features/cart/CartCheckout.tsx
@@ -1,7 +1,12 @@
 const CartCheckout = () => {
   return (
     <div className="mb-5">
-      <button>Checkout</button>
+      <button
+        className="w-full bg-orange-brown py-3 uppercase text-white text-[.9rem]
+       font-medium tracking-wider hover:bg-orange-brown-light transition-colors ease-in duration-150"
+      >
+        Checkout
+      </button>
     </div>
   );
 };
