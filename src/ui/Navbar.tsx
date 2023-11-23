50c197d49c24f9aaaa7e6ab6cbb5e9d2d8c97565
Sandro <vipsandro11@gmail.com>
2023-11-23 14:42:31 +0400

    chore: Refactor Navbar component

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,19 +1,27 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "/assets/logo.svg";
 import cart from "/assets/icon-cart.svg";
 import { setOpenCart } from "../features/cart/cartSlice";
 import { useAppDispatch } from "../hooks/reduxHooks";
 import CartItemCountPopup from "./CartItemCountPopup";
+import MobileNav from "./MobileNav";
+import MobileNavPop from "./MobileNavPop";
 const Navbar = () => {
   const dispatch = useAppDispatch();
+  const [isOpen, setOpen] = useState(false);
   return (
     <header className="w-full bg-primary-color py-10 relative after:absolute after:w-[72.375rem] after:mx-auto after:left-0 after:right-0 after:h-[1px] after:bg-divider after:bottom-0 ">
+      <MobileNavPop  isOpen={isOpen} />
       <nav className="section-center text-text-white">
-        <div className="flex justify-between">
-          <button>
-            <img src={logo} alt="logo" />
-          </button>
-          <ul className="flex-center gap-8 font-medium ">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-5">
+            <MobileNav isOpen={isOpen} setOpen={setOpen} />
+            <button>
+              <img src={logo} alt="logo" />
+            </button>
+          </div>
+          <ul className="gap-8 font-medium hidden lg:flex-center">
             <li className="text-[0.875rem] hover:text-orange-brown transition-colors ease-in duration-150 tracking-wider">
               <Link to="/">HOME</Link>
             </li>
