dc759fd25eabdd7dd642b371d9773f2ac4c2ff46
Sandro <vipsandro11@gmail.com>
2023-11-23 14:41:40 +0400

    refactor: Update CartItemCountPopup to display the total item count in the cart using totalCartAmount from cartSlice

--- src/ui/CartItemCountPopup.tsx
+++ src/ui/CartItemCountPopup.tsx
@@ -1,10 +1,11 @@
+import { totalCartAmount } from "../features/cart/cartSlice";
 import { useAppSelector } from "../hooks/reduxHooks";
 
 const CartItemCountPopup = () => {
-  const cart = useAppSelector((store) => store.cart.cart);
+  const totalAmount = useAppSelector(totalCartAmount);
   return (
     <div className="absolute -top-2 -right-[.6rem] bg-orange-brown w-5 h-5 rounded-full flex justify-center items-center">
-      <span className="text-[.7rem]">{cart.length}</span>
+      <span className="text-[.7rem]">{totalAmount}</span>
     </div>
   );
 };
