a869e997ade8fd77561b8852c9746d3c9328ea74
Sandro <vipsandro11@gmail.com>
2023-11-23 14:39:43 +0400

    feat: Add CartCounter component with Redux actions

--- src/features/cart/CartCounter.tsx
+++ src/features/cart/CartCounter.tsx
@@ -1,18 +1,24 @@
+import { useAppDispatch } from "../../hooks/reduxHooks";
+import { decreaseAmount, increasAmount } from "./cartSlice";
+
 type CounterTyps = {
   amount: number;
+  id: number;
 };
-const CartCounter = ({ amount }: CounterTyps) => {
+const CartCounter = ({ amount, id }: CounterTyps) => {
+  const dispatch = useAppDispatch();
+
   return (
     <div className="w-[5.5rem] h-[2rem] bg-color-gray inline-flex justify-center items-center">
       <button
-        // onClick={handleDecreaseAmount}
+        onClick={() => dispatch(decreaseAmount(id))}
         className="w-[50%] h-full text-[.7rem] mx-auto p-0 hover:bg-light-gray hover:text-orange-brown transition-colors ease-in duration-150"
       >
         -
       </button>
       <span className="px-2 text-[.7rem] font-medium">{amount}</span>
       <button
-        // onClick={handleIncreaseAmount}
+        onClick={() => dispatch(increasAmount(id))}
         className="w-[50%] h-full text-[.7rem] mx-auto p-0 hover:bg-light-gray hover:text-orange-brown transition-colors ease-in duration-150"
       >
         +
