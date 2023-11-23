d440f2bbd1ecc362e418a7f862eef1682e383520
Sandro <vipsandro11@gmail.com>
2023-11-23 14:41:13 +0400

    style: Adjust styling in SimilarProducts component for better responsiveness

--- src/features/singleProduct/SimilarProducts.tsx
+++ src/features/singleProduct/SimilarProducts.tsx
@@ -4,17 +4,31 @@ import SeeProductBtn from "../../ui/SeeProductBtn";
 const SimilarProducts = () => {
   const product = useAppSelector((store) => store.product.product);
   return (
-    <div className="flex gap-7 ">
+    <div className="flex flex-col md:flex-row gap-7 mb-[10rem]">
       {product.slice(0, 3).map((singleProduct) => {
         return (
           <div key={singleProduct.id}>
             <div className="bg-color-gray flex-1 rounded-md p-4">
-              <img src={singleProduct.image.desktop} alt="" />
+              <img
+                src={singleProduct.image.mobile}
+                alt=""
+                className="block md:hidden lg:hidden"
+              />
+              <img
+                src={singleProduct.image.tablet}
+                alt=""
+                className="hidden md:block lg:hidden"
+              />
+              <img
+                src={singleProduct.image.desktop}
+                alt=""
+                className="hidden lg:block"
+              />
             </div>
             <div className="text-center">
-              <h4 className="text-center my-5 font-medium">
-                {singleProduct.name}
-              </h4>
+              <h3 className="text-center my-5 font-medium">
+                {singleProduct.shortName}
+              </h3>
               <SeeProductBtn
                 bgClass="bg-orange-brown"
                 hoverClass="hover:bg-orange-brown-light"
