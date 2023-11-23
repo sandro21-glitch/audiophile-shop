81cb32cd05573c74a5bcc3239cf677bc40f66609
Sandro <vipsandro11@gmail.com>
2023-11-23 21:18:20 +0400

    style: add background overlay to MobileNavPop

--- src/ui/MobileNavPop.tsx
+++ src/ui/MobileNavPop.tsx
@@ -6,12 +6,20 @@ type NavProps = {
 
 const MobileNavPop = ({ isOpen }: NavProps) => {
   return (
-    <div
-      className={`lg:hidden bg-white absolute w-full z-[999] top-full ${
-        isOpen ? "translate-y-0" : "-translate-y-[200%]"
-      } rounded-b-3xl transition-all ease-linear duration-300 px-5`}
-    >
-      <Products />
+    <div>
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } lg:hidden fixed left-0 top-0 right-0 bg-black opacity-60
+         w-screen h-screen transition-all ease-in-out delay-500 duration-300`}
+      ></div>
+      <div
+        className={`lg:hidden bg-white absolute w-full z-[9999] top-full ${
+          isOpen ? "translate-y-0" : "-translate-y-[200%]"
+        } rounded-b-3xl transition-all ease-linear duration-300 px-5`}
+      >
+        <Products />
+      </div>
     </div>
   );
 };
