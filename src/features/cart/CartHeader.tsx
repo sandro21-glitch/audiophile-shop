d4fe561a0c778252e03ae708ee01adcf9008afcc
Sandro <vipsandro11@gmail.com>
2023-11-23 21:17:33 +0400

    feat: add toast confirmation on clearing cart

--- src/features/cart/CartHeader.tsx
+++ src/features/cart/CartHeader.tsx
@@ -1,16 +1,21 @@
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
 import { clearCart, totalCartAmount } from "./cartSlice";
-
+import toast from "react-hot-toast";
 const CartHeader = () => {
   const dispatch = useAppDispatch();
   const totalAmount = useAppSelector(totalCartAmount);
+
+  const handleClearCart = () => {
+    dispatch(clearCart());
+    toast.success("Cart is empty");
+  };
   return (
     <header className="flex justify-between w-full mb-10">
       <h6 className="text-black mb-0 uppercase font-medium text-[1.1rem]">
         Cart <span>({totalAmount})</span>
       </h6>
       <button
-        onClick={() => dispatch(clearCart())}
+        onClick={() => handleClearCart()}
         className="underline text-text text-[.9rem] hover:text-orange-brown transition-colors ease-in duration-150"
       >
         Remove All
