a2b8fe4f34d0e6f7cb9390cea0b77bda66671bbe
Sandro <vipsandro11@gmail.com>
2023-11-24 23:21:56 +0400

    style: Improve Navbar styling and manage body overflow for cart

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,21 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "/assets/logo.svg";
 import cart from "/assets/icon-cart.svg";
 import { setOpenCart } from "../features/cart/cartSlice";
-import { useAppDispatch } from "../hooks/reduxHooks";
+import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
 import CartItemCountPopup from "./CartItemCountPopup";
 import MobileNav from "./MobileNav";
 import MobileNavPop from "./MobileNavPop";
 const Navbar = () => {
   const dispatch = useAppDispatch();
   const [isOpen, setOpen] = useState(false);
+  const openCart = useAppSelector((store) => store.cart.openCart);
+  useEffect(() => {
+    if (openCart) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "visible";
+    }
+  }, [openCart]);
   return (
     <header className="w-full bg-primary-color py-10 relative after:absolute after:w-[72.375rem] after:mx-auto after:left-0 after:right-0 after:h-[1px] after:bg-divider after:bottom-0 ">
-      <MobileNavPop  isOpen={isOpen} />
-      <nav className="section-center text-text-white">
+      <MobileNavPop isOpen={isOpen} />
+      <nav className="section-center text-text-white ">
         <div className="flex justify-between items-center">
-          <div className="flex items-center gap-5">
+          <div className="flex items-center gap-5 static z-[9999]">
             <MobileNav isOpen={isOpen} setOpen={setOpen} />
             <button>
               <img src={logo} alt="logo" />
@@ -35,7 +43,7 @@ const Navbar = () => {
               <Link to="/earphones">EARPHONES</Link>
             </li>
           </ul>
-          <div className="relative">
+          <div className="relative  z-[9999]">
             <button onClick={() => dispatch(setOpenCart(true))}>
               <img src={cart} alt="cart" />
             </button>
