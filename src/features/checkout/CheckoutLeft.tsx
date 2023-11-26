cf84507d6727ecc2ae46fce48174aad72e370541
Sandro <vipsandro11@gmail.com>
2023-11-26 21:34:52 +0400

    feat: Integrate payment state in CheckoutLeft component

--- src/features/checkout/CheckoutLeft.tsx
+++ src/features/checkout/CheckoutLeft.tsx
@@ -1,15 +1,17 @@
+import { useState } from "react";
 import Billing from "./CheckoutLeft/Billing";
 import Payment from "./CheckoutLeft/Payment";
 import Shipping from "./CheckoutLeft/Shipping";
 
 const CheckoutLeft = () => {
+  const [payment, setPayment] = useState("e-money");
   return (
-    <section className="max-w-[45rem] p-10 bg-[#fff]">
+    <section className="w-full max-w-[45rem] p-10 bg-[#fff]">
       <form className="w-full ">
         <h1 className="text-[1.8rem] font-medium uppercase mb-10">CHECKOUT</h1>
         <Billing />
         <Shipping />
-        <Payment />
+        <Payment payment={payment} setPayment={setPayment} />
       </form>
     </section>
   );
