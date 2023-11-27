c52c9f38ee7f6a6af25cb2e72d522905a6d64376
Sandro <vipsandro11@gmail.com>
2023-11-27 23:33:38 +0400

    feat: Integrate checkout functionality in CartCheckout component

--- src/features/cart/CartCheckout.tsx
+++ src/features/cart/CartCheckout.tsx
@@ -1,7 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
+import { addCartItems } from "../checkout/checkoutSlice";
+import { setOpenCart } from "../cart/cartSlice";
 const CartCheckout = () => {
+  const dispatch = useAppDispatch();
+  const cart = useAppSelector((store) => store.cart.cart);
+  const navigate = useNavigate();
+  const handleCheckout = () => {
+    dispatch(addCartItems(cart));
+    dispatch(setOpenCart(false));
+    navigate("/checkout");
+  };
   return (
     <div className="mb-5">
       <button
+        onClick={() => handleCheckout()}
         className="w-full bg-orange-brown py-3 uppercase text-white text-[.9rem]
        font-medium tracking-wider hover:bg-orange-brown-light transition-colors ease-in duration-150"
       >
