4d200b4686234a41bc451481bf507dfa378feff3
Sandro <vipsandro11@gmail.com>
2023-11-28 20:14:03 +0400

    feat: Add shipping to checkout state and update totalSumVat calculation

--- src/features/checkout/checkoutSlice.ts
+++ src/features/checkout/checkoutSlice.ts
@@ -3,10 +3,12 @@ import { CartTypes } from "../cart/cartSlice";
 
 interface CheckoutState {
   checkout: CartTypes[];
+  shipping: number;
 }
 
 const initialState: CheckoutState = {
   checkout: [],
+  shipping: 50,
 };
 
 export const checkoutSlice = createSlice({
@@ -22,3 +24,29 @@ export const checkoutSlice = createSlice({
 export const { addCartItems } = checkoutSlice.actions;
 
 export default checkoutSlice.reducer;
+
+export const totalSumPrice = (state: { checkout: CheckoutState }) => {
+  const checkoutItems = state.checkout.checkout;
+  const totalAmount = checkoutItems.reduce((acc, item) => acc + item.price, 0);
+  return totalAmount;
+};
+// Function to calculate the total sum of VAT for all items in the cart
+export const totalSumVat = (state: { checkout: CheckoutState }) => {
+  const checkoutItems = state.checkout.checkout;
+
+  const totalVat = checkoutItems.reduce((acc, item) => {
+    const vatExcludedPrice = item.price;
+    // Calculating the VAT amount based on a 20% VAT rate
+    const vatAmount = vatExcludedPrice * 0.2;
+    return acc + vatAmount;
+  }, 0);
+  return totalVat;
+};
+export const grandTotalSum = (state: { checkout: CheckoutState }) => {
+  // Calling totalSumPrice and totalSumVat functions to get their values
+  const totalPrice = totalSumPrice(state);
+  const totalVat = totalSumVat(state);
+
+  // Adding shipping, total price, and total VAT to get the grand total
+  return totalPrice + state.checkout.shipping + totalVat;
+};
