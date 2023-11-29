c66268474548a4589183c6d216cfcf491b080055
Sandro <vipsandro11@gmail.com>
2023-11-29 16:43:41 +0400

    feat: Update InputField component to handle input value and onChange event

--- src/ui/InputField.tsx
+++ src/ui/InputField.tsx
@@ -2,9 +2,17 @@ interface InputFieldProps {
   label: string;
   id: string;
   placeholder: string;
+  value: string;
+  event: (value: string) => void;
 }
 
-const InputField = ({ label, id, placeholder }: InputFieldProps) => {
+const InputField = ({
+  label,
+  id,
+  placeholder,
+  value,
+  event,
+}: InputFieldProps) => {
   return (
     <div className="flex flex-col w-full">
       <label htmlFor={id} className="text-[.7rem] font-medium capitalize mb-2">
@@ -14,6 +22,8 @@ const InputField = ({ label, id, placeholder }: InputFieldProps) => {
         type="text"
         id={id}
         placeholder={placeholder}
+        value={value}
+        onChange={(e) => event(e.target.value)}
         className="w-full border p-3 rounded-lg border-text outline-none focus:border-orange-brown transition-all ease-in duration-150"
       />
     </div>
