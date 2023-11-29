a622d65c8690b4ff1f77ede65ef6a369dfc74aed
Sandro <vipsandro11@gmail.com>
2023-11-29 16:43:54 +0400

    feat: Update formatPrice function to allow one decimal place in the formatted price

--- src/utils/formatPrice.tsx
+++ src/utils/formatPrice.tsx
@@ -1,11 +1,14 @@
 export const formatPrice = (number: number) => {
+  // Round the number to one decimal place
+  const roundedNumber = Math.round(number * 10) / 10;
+
   const formattedPrice = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  }).format(number);
+    maximumFractionDigits: 1, // Allow one decimal place
+  }).format(roundedNumber);
 
-  // Remove the ".00" if present
-  return formattedPrice.replace(/\.00$/, "");
+  // Remove the ".0" if present
+  return formattedPrice.replace(/\.0$/, "");
 };
