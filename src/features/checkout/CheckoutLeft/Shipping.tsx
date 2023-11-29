9039e9a378354abd73f0128040c982cba1e95e95
Sandro <vipsandro11@gmail.com>
2023-11-29 16:43:04 +0400

    feat: Update Shipping component to handle input values and state using props

--- src/features/checkout/CheckoutLeft/Shipping.tsx
+++ src/features/checkout/CheckoutLeft/Shipping.tsx
@@ -1,6 +1,23 @@
 import InputField from "../../../ui/InputField";
+type ShippingTypes = {
+  shipping: { address: string; zip: string; city: string; country: string };
+  setShipping: React.Dispatch<
+    React.SetStateAction<{
+      address: string;
+      zip: string;
+      city: string;
+      country: string;
+    }>
+  >;
+};
+const Shipping = ({ shipping, setShipping }: ShippingTypes) => {
+  const handleInputChange = (field: string, value: string) => {
+    setShipping((prevShipping) => ({
+      ...prevShipping,
+      [field]: value,
+    }));
+  };
 
-const Shipping = () => {
   return (
     <fieldset className="w-full mb-10">
       <legend className="uppercase text-orange-brown font-medium text-[.9rem] mb-3 tracking-wider">
@@ -11,10 +28,30 @@ const Shipping = () => {
           label="Your Address"
           id="address"
           placeholder="1137 Williams Avenue"
+          value={shipping.address}
+          event={(value) => handleInputChange("address", value)}
         />
-        <InputField label="ZIP Code" id="zip" placeholder="10001" />
-        <InputField label="City" id="city" placeholder="New York" />
-        <InputField label="Country" id="country" placeholder="United States" />
+        <InputField
+          label="ZIP Code"
+          id="zip"
+          placeholder="10001"
+          value={shipping.zip}
+          event={(value) => handleInputChange("zip", value)}
+        />
+        <InputField
+          label="City"
+          id="city"
+          placeholder="New York"
+          value={shipping.city}
+          event={(value) => handleInputChange("city", value)}
+        />
+        <InputField
+          label="Country"
+          id="country"
+          placeholder="United States"
+          value={shipping.country}
+          event={(value) => handleInputChange("country", value)}
+        />
       </div>
     </fieldset>
   );
