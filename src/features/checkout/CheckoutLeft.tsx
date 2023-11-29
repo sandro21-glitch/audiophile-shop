df9aa647a05e424601afa82ea6d6b90c47c9c6b5
Sandro <vipsandro11@gmail.com>
2023-11-29 16:41:27 +0400

    feat: Expand CheckoutLeft component state

--- src/features/checkout/CheckoutLeft.tsx
+++ src/features/checkout/CheckoutLeft.tsx
@@ -5,12 +5,19 @@ import Shipping from "./CheckoutLeft/Shipping";
 
 const CheckoutLeft = () => {
   const [payment, setPayment] = useState("e-money");
+  const [billing, setBilling] = useState({ name: "", email: "", number: "" });
+  const [shipping, setShipping] = useState({
+    address: "",
+    zip: "",
+    city: "",
+    country: "",
+  });
   return (
     <section className="w-full max-w-[45rem] p-10 bg-[#fff]">
       <form className="w-full ">
         <h1 className="text-[1.8rem] font-medium uppercase mb-10">CHECKOUT</h1>
-        <Billing />
-        <Shipping />
+        <Billing billing={billing} setBilling={setBilling} />
+        <Shipping shipping={shipping} setShipping={setShipping} />
         <Payment payment={payment} setPayment={setPayment} />
       </form>
     </section>
