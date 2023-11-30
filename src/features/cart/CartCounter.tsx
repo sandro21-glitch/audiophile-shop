e0335aeaad46e96d541601172baad405ecb0ef50
Sandro <vipsandro11@gmail.com>
2023-11-30 15:15:13 +0400

    feat: Enhance CartCounter component functionality

--- src/features/cart/CartCounter.tsx
+++ src/features/cart/CartCounter.tsx
@@ -1,4 +1,6 @@
-import { useAppDispatch } from "../../hooks/reduxHooks";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
+import { addCartItems } from "../checkout/checkoutSlice";
 import { decreaseAmount, increasAmount } from "./cartSlice";
 
 type CounterTyps = {
@@ -7,18 +9,27 @@ type CounterTyps = {
 };
 const CartCounter = ({ amount, id }: CounterTyps) => {
   const dispatch = useAppDispatch();
-
+  const cart = useAppSelector((store) => store.cart.cart);
+  const handleDecreaseAmount = () => {
+    dispatch(decreaseAmount(id));
+  };
+  const handleIncreaseAmount = () => {
+    dispatch(increasAmount(id));
+  };
+  useEffect(() => {
+    dispatch(addCartItems(cart));
+  }, [cart]);
   return (
     <div className="w-[5.5rem] h-[2rem] bg-color-gray inline-flex justify-center items-center">
       <button
-        onClick={() => dispatch(decreaseAmount(id))}
+        onClick={() => handleDecreaseAmount()}
         className="w-[50%] h-full text-[.7rem] mx-auto p-0 hover:bg-light-gray hover:text-orange-brown transition-colors ease-in duration-150"
       >
         -
       </button>
       <span className="px-2 text-[.7rem] font-medium">{amount}</span>
       <button
-        onClick={() => dispatch(increasAmount(id))}
+        onClick={() => handleIncreaseAmount()}
         className="w-[50%] h-full text-[.7rem] mx-auto p-0 hover:bg-light-gray hover:text-orange-brown transition-colors ease-in duration-150"
       >
         +
