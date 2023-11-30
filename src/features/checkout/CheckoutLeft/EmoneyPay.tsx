348f15496860030afd209cf47393bc30d23ac659
Sandro <vipsandro11@gmail.com>
2023-11-30 16:29:14 +0400

    style: Add font-medium class to EmoneyPay component labels

--- src/features/checkout/CheckoutLeft/EmoneyPay.tsx
+++ src/features/checkout/CheckoutLeft/EmoneyPay.tsx
@@ -1,7 +1,7 @@
 const EmoneyPay = () => {
   return (
     <div className="flex gap-5">
-      <label htmlFor="e-number" className="w-full text-[.7rem] text-black">
+      <label htmlFor="e-number" className="w-full text-[.7rem] text-black font-medium">
         e-Money Number
         <input
           id="e-number"
@@ -10,7 +10,7 @@ const EmoneyPay = () => {
           placeholder="231242442"
         />
       </label>
-      <label htmlFor="pin" className="w-full text-[.7rem] text-black">
+      <label htmlFor="pin" className="w-full text-[.7rem] text-black font-medium">
         e-Money PIN
         <input
           id="pin"
