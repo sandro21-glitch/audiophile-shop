199dede54688b84db6a45915949735b2bbc8a428
Sandro <vipsandro11@gmail.com>
2023-11-30 15:15:52 +0400

    feat: Integrate addCartItems in Summary component

--- src/features/checkout/Summary.tsx
+++ src/features/checkout/Summary.tsx
@@ -1,22 +1,31 @@
-import { useAppSelector } from "../../hooks/reduxHooks";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
 import GrandTotal from "./summary/GrandTotal";
 import ShippingSum from "./summary/ShippingSum";
 import SumProduct from "./summary/SumProduct";
 import TotalPrice from "./summary/TotalPrice";
 import TotalVat from "./summary/TotalVat";
+import { addCartItems } from "./checkoutSlice";
 
 const Summary = () => {
+  const dispatch = useAppDispatch();
   const checkout = useAppSelector((store) => store.checkout.checkout);
+  const cart = useAppSelector((store) => store.cart.cart);
+  useEffect(() => {
+    dispatch(addCartItems(cart));
+  }, [cart]);
   return (
     <section className="bg-white flex-1 p-10">
       <h2 className="uppercase text-[1.3rem] font-medium tracking-wider mb-5">
         Summary
       </h2>
-      <ul>
-        {checkout.map((product) => {
-          return <SumProduct key={product.id} product={product} />;
-        })}
-      </ul>
+      {checkout && (
+        <ul>
+          {checkout.map((product) => {
+            return <SumProduct key={product.id} product={product} />;
+          })}
+        </ul>
+      )}
       <TotalPrice />
       <ShippingSum />
       <TotalVat />
