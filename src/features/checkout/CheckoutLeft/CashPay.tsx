056b1657b7d98a911a95057d3d3f990bcaf0c816
Sandro <vipsandro11@gmail.com>
2023-11-30 16:28:56 +0400

    feat: Enhance CashPay component with Cash on Delivery icon

--- src/features/checkout/CheckoutLeft/CashPay.tsx
+++ src/features/checkout/CheckoutLeft/CashPay.tsx
@@ -1,9 +1,16 @@
-
+import cash from "/images/checkout/icon-cash-on-delivery.svg";
 
 const CashPay = () => {
   return (
-    <div>CashPay</div>
-  )
-}
+    <div className="flex items-center gap-10">
+      <img src={cash} alt="" className="w-[3rem] h-[3rem]" />
+      <p className="text-text text-[.9rem]">
+        The ‘Cash on Delivery’ option enables you to pay in cash when our
+        delivery courier arrives at your residence. Just make sure your address
+        is correct so that your order will not be cancelled.
+      </p>
+    </div>
+  );
+};
 
-export default CashPay
+export default CashPay;
