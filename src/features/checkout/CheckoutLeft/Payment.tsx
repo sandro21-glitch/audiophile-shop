6057795e3b8a799279e894b4e18b228f01a8fd50
Sandro <vipsandro11@gmail.com>
2023-11-30 16:28:35 +0400

    refactor: Update Payment component UI for better accessibility

--- src/features/checkout/CheckoutLeft/Payment.tsx
+++ src/features/checkout/CheckoutLeft/Payment.tsx
@@ -8,50 +8,58 @@ type PaymentTypes = {
 };
 
 const Payment = ({ payment, setPayment }: PaymentTypes) => {
-  const handleUpdatePaymentMethod = (method: string) => {
-    setPayment(method);
-  };
-
-  // PaymentOption component for individual payment options
-  const PaymentOption: React.FC<{ id: string; label: string }> = ({
-    id,
-    label,
-  }) => (
-    <div
-      onChange={() => handleUpdatePaymentMethod(label)}
-      className="p-4 border flex items-center gap-3 cursor-pointer mb-5 rounded-lg"
-    >
-      <input
-        type="checkbox"
-        name={id}
-        id={id}
-        className="cursor-pointer"
-        checked={payment === label}
-      />
-      <label htmlFor={id} className="cursor-pointer font-medium text-[.9rem]">
-        {label}
-      </label>
-    </div>
-  );
-
   return (
     <fieldset className="w-full mb-10">
       {/* Payment details legend */}
       <legend className="uppercase text-orange-brown font-medium text-[.9rem] mb-3 tracking-wider">
         PAYMENT DETAILS
       </legend>
-      <div className="flex justify-between">
+      <div className="flex justify-between mb-7">
         {/* Payment method label */}
         <p className="text-black text-[.7rem] font-medium flex-1">
           Payment Method
         </p>
         <div className="flex-1">
           {/* Render e-Money and Cash on Delivery payment options */}
-          <PaymentOption id="e-money" label="e-Money" />
-          <PaymentOption id="cash" label="Cash on Delivery" />
+          <div className="flex flex-col gap-5">
+            <label
+              htmlFor="e-money"
+              className={`${
+                payment === "e-money"
+                  ? "border-orange-brown"
+                  : "border-gray-200"
+              } border p-4 rounded-lg flex text-sm font-medium cursor-pointer`}
+            >
+              <input
+                type="radio"
+                id="e-money"
+                value="e-money"
+                checked={payment === "e-money"}
+                onChange={(e) => setPayment(e.target.value)}
+                className="mr-5"
+              />
+              e-Money
+            </label>
+            <label
+              htmlFor="cash"
+              className={`${
+                payment === "cash" ? "border-orange-brown" : "border-gray-200"
+              } border p-4 rounded-lg flex text-sm font-medium cursor-pointer`}
+            >
+              <input
+                type="radio"
+                value="cash"
+                id="cash"
+                checked={payment === "cash"}
+                onChange={(e) => setPayment(e.target.value)}
+                className="mr-5"
+              />
+              Cash on Delivery
+            </label>
+          </div>
         </div>
       </div>
-        {payment === "e-money" ? <EmoneyPay /> : <CashPay />}
+      {payment === "e-money" ? <EmoneyPay /> : <CashPay />}
     </fieldset>
   );
 };
