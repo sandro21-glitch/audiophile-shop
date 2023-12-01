c43c15f20f7c9b84233728754996efe5902590fb
Sandro <vipsandro11@gmail.com>
2023-12-01 19:54:01 +0400

    refactor: Update CheckoutLeft to use TypeScript and specific prop types

--- src/features/checkout/CheckoutLeft.tsx
+++ src/features/checkout/CheckoutLeft.tsx
@@ -1,25 +1,41 @@
-import { useState } from "react";
 import Billing from "./CheckoutLeft/Billing";
 import Payment from "./CheckoutLeft/Payment";
 import Shipping from "./CheckoutLeft/Shipping";
 
-const CheckoutLeft = () => {
-  const [payment, setPayment] = useState("e-money");
-  const [billing, setBilling] = useState({ name: "", email: "", number: "" });
-  const [shipping, setShipping] = useState({
-    address: "",
-    zip: "",
-    city: "",
-    country: "",
-  });
+type CheckoutTypes = {
+  billing: { name: string; email: string; number: string };
+  setBilling: React.Dispatch<React.SetStateAction<{ name: string; email: string; number: string }>>;
+  shipping: { address: string; zip: string; city: string; country: string };
+  setShipping: React.Dispatch<React.SetStateAction<{ address: string; zip: string; city: string; country: string }>>;
+  payment: string;
+  setPayment: React.Dispatch<React.SetStateAction<string>>;
+  emoneyOptions: { emoneyNumber: string; pin: string };
+  setEmoneyOptions: React.Dispatch<React.SetStateAction<{ emoneyNumber: string; pin: string }>>;
+};
+
+const CheckoutLeft = ({
+  billing,
+  setBilling,
+  shipping,
+  setShipping,
+  payment,
+  setPayment,
+  emoneyOptions,
+  setEmoneyOptions,
+}: CheckoutTypes) => {
   return (
     <section className="w-full max-w-[45rem] p-10 bg-[#fff]">
-      <form className="w-full ">
+      <div className="w-full">
         <h1 className="text-[1.8rem] font-medium uppercase mb-10">CHECKOUT</h1>
         <Billing billing={billing} setBilling={setBilling} />
         <Shipping shipping={shipping} setShipping={setShipping} />
-        <Payment payment={payment} setPayment={setPayment} />
-      </form>
+        <Payment
+          payment={payment}
+          setPayment={setPayment}
+          emoneyOptions={emoneyOptions}
+          setEmoneyOptions={setEmoneyOptions}
+        />
+      </div>
     </section>
   );
 };
