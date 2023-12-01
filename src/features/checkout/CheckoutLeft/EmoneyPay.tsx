bf5ed0cf56657b5f217521ec8df7f9f515637228
Sandro <vipsandro11@gmail.com>
2023-12-01 19:55:34 +0400

    refactor: Refactor EmoneyPay component to use controlled inputs for better state management

--- src/features/checkout/CheckoutLeft/EmoneyPay.tsx
+++ src/features/checkout/CheckoutLeft/EmoneyPay.tsx
@@ -1,20 +1,49 @@
-const EmoneyPay = () => {
+type EmoneyTypes = {
+  emoneyOptions: { emoneyNumber: string; pin: string };
+  setEmoneyOptions: React.Dispatch<
+    React.SetStateAction<{
+      emoneyNumber: string;
+      pin: string;
+    }>
+  >;
+};
+const EmoneyPay = ({ emoneyOptions, setEmoneyOptions }: EmoneyTypes) => {
   return (
     <div className="flex gap-5">
-      <label htmlFor="e-number" className="w-full text-[.7rem] text-black font-medium">
+      <label
+        htmlFor="e-number"
+        className="w-full text-[.7rem] text-black font-medium"
+      >
         e-Money Number
         <input
           id="e-number"
           type="text"
+          value={emoneyOptions.emoneyNumber}
+          onChange={(e) =>
+            setEmoneyOptions((prev) => ({
+              ...prev,
+              emoneyNumber: e.target.value,
+            }))
+          }
           className="w-full border p-4 rounded-lg outline-none mt-2 font-medium"
           placeholder="231242442"
         />
       </label>
-      <label htmlFor="pin" className="w-full text-[.7rem] text-black font-medium">
+      <label
+        htmlFor="pin"
+        className="w-full text-[.7rem] text-black font-medium"
+      >
         e-Money PIN
         <input
           id="pin"
           type="text"
+          value={emoneyOptions.pin}
+          onChange={(e) =>
+            setEmoneyOptions((prev) => ({
+              ...prev,
+              pin: e.target.value,
+            }))
+          }
           className="w-full border p-4 rounded-lg outline-none mt-2 font-medium"
           placeholder="3424"
         />
