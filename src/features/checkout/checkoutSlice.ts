117cdb69b255f3ec8e21e48a4396fad8173fe360
Sandro <vipsandro11@gmail.com>
2023-12-04 14:55:08 +0400

    refactor: Update checkout state and add cart completion flag

--- src/features/checkout/checkoutSlice.ts
+++ src/features/checkout/checkoutSlice.ts
@@ -1,56 +1,61 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { CartTypes } from "../cart/cartSlice";
+  import { createSlice } from "@reduxjs/toolkit";
+  import { CartTypes } from "../cart/cartSlice";
 
-interface CheckoutState {
-  checkout: CartTypes[];
-  shipping: number;
-}
+  interface CheckoutState {
+    checkout: CartTypes[];
+    shipping: number;
+    complete: boolean;
+  }
 
-const initialState: CheckoutState = {
-  checkout: [],
-  shipping: 50,
-};
+  const initialState: CheckoutState = {
+    checkout: [],
+    shipping: 50,
+    complete: false,
+  };
 
-export const checkoutSlice = createSlice({
-  name: "checkout",
-  initialState,
-  reducers: {
-    addCartItems: (state, action) => {
-      state.checkout = action.payload;
+  export const checkoutSlice = createSlice({
+    name: "checkout",
+    initialState,
+    reducers: {
+      addCartItems: (state, action) => {
+        state.checkout = action.payload;
+      },
+      setCartPopup: (state, action) => {
+        state.complete = action.payload;
+      },
     },
-  },
-});
+  });
 
-export const { addCartItems } = checkoutSlice.actions;
+  export const { addCartItems, setCartPopup } = checkoutSlice.actions;
 
-export default checkoutSlice.reducer;
+  export default checkoutSlice.reducer;
 
-export const totalSumPrice = (state: { checkout: CheckoutState }) => {
-  const checkoutItems = state.checkout.checkout;
-  const totalAmount = checkoutItems.reduce(
-    (acc, item) => acc + item.price * item.amount,
-    0
-  );
-  return totalAmount;
-};
-// Function to calculate the total sum of VAT for all items in the cart
-export const totalSumVat = (state: { checkout: CheckoutState }) => {
-  const checkoutItems = state.checkout.checkout;
+  export const totalSumPrice = (state: { checkout: CheckoutState }) => {
+    const checkoutItems = state.checkout.checkout;
+    const totalAmount = checkoutItems.reduce(
+      (acc, item) => acc + item.price * item.amount,
+      0
+    );
+    return totalAmount;
+  };
+  // Function to calculate the total sum of VAT for all items in the cart
+  export const totalSumVat = (state: { checkout: CheckoutState }) => {
+    const checkoutItems = state.checkout.checkout;
 
-  const totalVat = checkoutItems.reduce((acc, item) => {
-    const vatExcludedPrice = item.price * item.amount;
-    // calculating the VAT amount based on a 20% VAT rate
-    const vatAmount = vatExcludedPrice * 0.2;
-    return acc + vatAmount;
-  }, 0);
+    const totalVat = checkoutItems.reduce((acc, item) => {
+      const vatExcludedPrice = item.price * item.amount;
+      // calculating the VAT amount based on a 20% VAT rate
+      const vatAmount = vatExcludedPrice * 0.2;
+      return acc + vatAmount;
+    }, 0);
 
-  return totalVat;
-};
-export const grandTotalSum = (state: { checkout: CheckoutState }) => {
-  // Calling totalSumPrice and totalSumVat functions to get their values
-  const totalPrice = totalSumPrice(state);
-  const totalVat = totalSumVat(state);
+    return totalVat;
+  };
+  export const grandTotalSum = (state: { checkout: CheckoutState }) => {
+    // Calling totalSumPrice and totalSumVat functions to get their values
+    const totalPrice = totalSumPrice(state);
+    const totalVat = totalSumVat(state);
 
-  // Adding shipping, total price, and total VAT to get the grand total
-  return totalPrice + state.checkout.shipping + totalVat;
-};
+    // Adding shipping, total price, and total VAT to get the grand total
+    return totalPrice + state.checkout.shipping + totalVat;
+  };
