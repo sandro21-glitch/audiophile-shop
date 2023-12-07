eb9bba54f08854e496896ce01e9729dc14426e2b
Sandro <vipsandro11@gmail.com>
2023-12-07 17:22:48 +0400

    refactor: Add onClick handler to close menu in Products component

--- src/features/home/Products.tsx
+++ src/features/home/Products.tsx
@@ -4,14 +4,25 @@ import speakers from "/assets/image-speakers.png";
 import earphones from "/assets/image-earphones.png";
 import arrowRight from "/assets/icon-arrow-right.svg";
 import { motion } from "framer-motion";
-const Products = () => {
+type ProductsProps = {
+  setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+};
+const Products = ({ setOpen }: ProductsProps) => {
+  const handleItemClick = () => {
+    if (setOpen) {
+      setOpen(false);
+    }
+  };
   return (
     <motion.ul
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 2 } }}
       className="my-20 lg:my-28 flex items-center justify-between gap-7"
     >
-      <li className="group pt-[5.5rem] pb-[1.5rem] cursor-pointer relative flex-1 bg-color-gray rounded-lg flex flex-col justify-center items-center">
+      <li
+        onClick={handleItemClick}
+        className="group pt-[5.5rem] pb-[1.5rem] cursor-pointer relative flex-1 bg-color-gray rounded-lg flex flex-col justify-center items-center"
+      >
         <Link
           to="/headphones"
           className="flex flex-col justify-center items-center"
@@ -32,7 +43,10 @@ const Products = () => {
           </div>
         </Link>
       </li>
-      <li className="group pt-[5.5rem] pb-[1.5rem] cursor-pointer relative flex-1 bg-color-gray rounded-lg flex flex-col justify-center items-center">
+      <li
+        onClick={handleItemClick}
+        className="group pt-[5.5rem] pb-[1.5rem] cursor-pointer relative flex-1 bg-color-gray rounded-lg flex flex-col justify-center items-center"
+      >
         <Link
           to="/speakers"
           className="flex flex-col justify-center items-center"
@@ -53,7 +67,10 @@ const Products = () => {
           </div>
         </Link>
       </li>
-      <li className="group pt-[5.5rem] pb-[1.5rem] cursor-pointer relative flex-1 bg-color-gray rounded-lg flex flex-col justify-center items-center">
+      <li
+        onClick={handleItemClick}
+        className="group pt-[5.5rem] pb-[1.5rem] cursor-pointer relative flex-1 bg-color-gray rounded-lg flex flex-col justify-center items-center"
+      >
         <Link
           to="/earphones"
           className="flex flex-col justify-center items-center"
